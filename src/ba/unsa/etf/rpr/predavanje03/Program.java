96b5cf13d2a7fbfdc5af9efdbeabcb9170cf9875
szaimovic1 <szaimovic1@etf.unsa.ba>
2018-10-24 03:33:46 +0200

    testovi dodani

---
+++ src/ba/unsa/etf/rpr/predavanje03/Program.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.rpr.predavanje03;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+    }
+
+}
+
